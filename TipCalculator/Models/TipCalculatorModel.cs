9a35338125dae3177ecf73f4dfdc774c2e2621e1
brome40 <85357500+brome40@users.noreply.github.com>
2023-04-06 21:00:53 -0400

    Add project files.

---
+++ TipCalculator/Models/TipCalculatorModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Models
+{
+    public class TipCalculatorModel
+    {
+        [Required(ErrorMessage = "Please enter a meal cost.")]
+        [Range(0.0, Int32.MaxValue, ErrorMessage =
+               "Meal cost must be greater than 0.")]
+        public decimal? MealCost { get; set; }
+
+        public decimal? CalculateFifteenTip()
+        {
+            return MealCost * 0.15m;
+        }
+
+        public decimal? CalculateTwentyTip()
+        {
+            return MealCost * 0.20m;
+        }
+
+        public decimal? CalculateTwentyFiveTip()
+        {
+            return MealCost * 0.25m;
+        }
+    }
+}
